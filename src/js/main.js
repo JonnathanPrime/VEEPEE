16c9cc9cebc28241338169d3253b12eb85d31817
jonnathan guzman <jonnathanguzman@MacBook-Pro-de-jonnathan.local>
2020-10-26 16:23:02 +0100

    slider lvl up

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,24 @@
 //JSOn parse 
 var obj = JSON.parse(JSON.stringify(text));
 console.log(text);
-country(obj);
-place(obj);
-var villes;
-function country(JsonObj) {
-  villes =JsonObj['country'];
 
-  for(var i = 0; i < villes.length; i++) {
-    country.textContent = villes[i].country;
-  }
-};
+
+document.getElementById("pays").innerHTML =
+obj.destinations[0].country;
+
+document.getElementById("ville").innerHTML =
+obj.destinations[0].place;
+
+document.getElementById("prix").innerHTML =
+obj.destinations[0].upto;
+
+document.getElementById("label").innerHTML =
+obj.destinations[0].label;
+
+document.getElementById("tag1").innerHTML =
+obj.destinations[0].tags[0].label;
+
+document.getElementById("tag2").innerHTML =
+obj.destinations[0].tags[1].label;
+
 
